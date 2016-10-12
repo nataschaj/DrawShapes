4a166c1ce6c64c92094f102fa8fe7ae459289480
Martin Kierkegaard <mark@easj.dk>
2016-10-12 20:27:00 +0200

    Add project files.

---
+++ Sandbox/DrawingTool.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    // A class for very primitive drawing...
+    class DrawingTool
+    {
+        public DrawingTool()
+        {
+        }
+
+        public void DrawOneStar()
+        {
+            Console.Write("*");
+        }
+
+        public void DrawOneSpace()
+        {
+            Console.Write(" ");
+        }
+
+        public void StartNewLine()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sandbox
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            DrawingTool drawer = new DrawingTool();
+
+
+            // YOUR JOB: Add code to draw shapes
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
