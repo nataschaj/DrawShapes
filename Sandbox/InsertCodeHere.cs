dc644caa8583c51a6290e32a06c98730b4f18bf4
Martin Kierkegaard <mark@easj.dk>
2016-10-12 20:31:32 +0200

    solution for teh exercise

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -10,9 +10,65 @@ namespace Sandbox
 
             DrawingTool drawer = new DrawingTool();
 
+            // Part 2a
+            Console.WriteLine("Solution for 2a");
+            for (int x = 0; x < 10; x++)
+            {
+                drawer.DrawOneStar();
+            }
+            Console.WriteLine();
 
-            // YOUR JOB: Add code to draw shapes
+            // Part 2b
+            Console.WriteLine("Solution for 2b");
+            for (int x = 0; x < 5; x++)
+            {
+                drawer.DrawOneStar();
+                drawer.DrawOneSpace();
+            }
+            Console.WriteLine();
 
+            // Part 2c
+            Console.WriteLine("Solution for 2c");
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    drawer.DrawOneStar();
+                }
+                drawer.StartNewLine();
+            }
+            Console.WriteLine();
+
+            // Part 2d
+            Console.WriteLine("Solution for 2d");
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x <= y; x++)
+                {
+                    drawer.DrawOneStar();
+                }
+                drawer.StartNewLine();
+            }
+            Console.WriteLine();
+
+            // Part 2e
+            Console.WriteLine("Solution for 2e");
+            for (int y = 0; y < 10; y++)
+            {
+                for (int x = 0; x < 10; x++)
+                {
+                    if ((x == y) || (x + y == 9))
+                    {
+                        drawer.DrawOneStar();
+                    }
+                    else
+                    {
+                        drawer.DrawOneSpace();
+                    }
+                }
+                drawer.StartNewLine();
+            }
+            Console.WriteLine();
 
             // The LAST line of code should be ABOVE this line
         }
