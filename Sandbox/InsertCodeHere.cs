783a4c0f702c7497a7c97cb8129a58c784b18869
Natascha Jakobsen <natascha_jakobsen@hotmail.com>
2016-10-31 14:18:29 +0100

    M

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -12,7 +12,10 @@ namespace Sandbox
 
 
             // YOUR JOB: Add code to draw shapes
+            for (int i = 0; i < length; i++)
+            {
 
+            }
 
             // The LAST line of code should be ABOVE this line
         }
